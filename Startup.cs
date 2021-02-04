4a4f4082547f2b7ed761908753b191f1e19baf4c
Orkhan Jamaladdinov <orxan.camaleddinov@gmail.com>
2021-02-04 20:47:26 +0200

    Seeding Database

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MeetupAPI.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,10 +27,12 @@ namespace MeetupAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<MeetupContext>();
+            services.AddScoped<MeetupSeeder>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MeetupSeeder meetupSeeder)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +49,8 @@ namespace MeetupAPI
             {
                 endpoints.MapControllers();
             });
+
+            meetupSeeder.Seed();
         }
     }
 }
